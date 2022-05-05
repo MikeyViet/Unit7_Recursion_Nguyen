764bfadad5b949c6c91ebae8aec5c3b7fa2a1bb9
Michael Nguyen <98675293+MikeyViet@users.noreply.github.com>
2022-05-04 23:54:46 -0500

    Finished Fact()

--- Unit7_Recursion_Nguyen.cpp
+++ Unit7_Recursion_Nguyen.cpp
@@ -2,9 +2,28 @@
 
 using namespace std;
 
+int fact(int);
+
 int main()
 {
-    cout << "Testing " << endl;
+    int number = 0;
+
+    cout << "Please enter a number: ";
+    cin >> number;
+
+    cout << "The factorial of " << number << " is " << fact(number) << endl;
 
     return 0;
+}
+
+int fact(int num)
+{
+    if(num > 1)
+    {
+        return num * fact(num - 1);
+    }
+    else
+    {
+        return 1;
+    }
 }
